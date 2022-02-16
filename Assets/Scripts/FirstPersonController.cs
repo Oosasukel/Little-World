027c2de907a93e38ff674e7b251c047e138f1e4f
Oosasukel <rodrigo_gonn@hotmail.com>
2022-02-16 13:07:22 -0300

    Gravity

--- Assets/Scripts/FirstPersonController.cs
+++ Assets/Scripts/FirstPersonController.cs
@@ -4,77 +4,27 @@ using UnityEngine;
 [RequireComponent(typeof(GravityBody))]
 public class FirstPersonController : MonoBehaviour
 {
-
-    // public vars
     public float mouseSensitivityX = 1;
-    public float mouseSensitivityY = 1;
-    public float walkSpeed = 6;
-    public float jumpForce = 220;
-    public LayerMask groundedMask;
+    public float moveSpeed = 15;
+    private Vector3 moveDir;
+    private Rigidbody rb;
 
-    // System vars
-    [SerializeField]
-    bool grounded;
-    Vector3 moveAmount;
-    Vector3 smoothMoveVelocity;
-    float verticalLookRotation;
-    Transform cameraTransform;
-    Rigidbody rigidbody;
-
-
-    void Awake()
+    void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-        cameraTransform = Camera.main.transform;
-        rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
     void Update()
     {
-
         // Look rotation:
         transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * mouseSensitivityX);
-        verticalLookRotation += Input.GetAxis("Mouse Y") * mouseSensitivityY;
-        verticalLookRotation = Mathf.Clamp(verticalLookRotation, -60, 60);
-        cameraTransform.localEulerAngles = Vector3.left * verticalLookRotation;
 
-        // Calculate movement:
-        float inputX = Input.GetAxisRaw("Horizontal");
-        float inputY = Input.GetAxisRaw("Vertical");
-
-        Vector3 moveDir = new Vector3(inputX, 0, inputY).normalized;
-        Vector3 targetMoveAmount = moveDir * walkSpeed;
-        moveAmount = Vector3.SmoothDamp(moveAmount, targetMoveAmount, ref smoothMoveVelocity, .15f);
-
-        // Jump
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (grounded)
-            {
-                rigidbody.AddForce(transform.up * jumpForce);
-            }
-        }
-
-        // Grounded check
-        Ray ray = new Ray(transform.position, -transform.up);
-        RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, 1 + .1f, groundedMask))
-        {
-            grounded = true;
-        }
-        else
-        {
-            grounded = false;
-        }
-
+        moveDir = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")).normalized;
     }
 
     void FixedUpdate()
     {
-        // Apply movement to rigidbody
-        Vector3 localMove = transform.TransformDirection(moveAmount) * Time.fixedDeltaTime;
-        rigidbody.MovePosition(rigidbody.position + localMove);
+        rb.MovePosition(rb.position + transform.TransformDirection(moveDir) * moveSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/GravityBody.cs
+++ Assets/Scripts/GravityBody.cs
@@ -1,23 +1,20 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 
-[RequireComponent (typeof (Rigidbody))]
-public class GravityBody : MonoBehaviour {
-	
-	GravityAttractor planet;
-	Rigidbody rigidbody;
-	
-	void Awake () {
-		planet = GameObject.FindGameObjectWithTag("Planet").GetComponent<GravityAttractor>();
-		rigidbody = GetComponent<Rigidbody> ();
+[RequireComponent(typeof(Rigidbody))]
+public class GravityBody : MonoBehaviour
+{
+    public GravityAttractor attractor;
+    private Rigidbody rb;
 
-		// Disable rigidbody gravity and rotation as this is simulated in GravityAttractor script
-		rigidbody.useGravity = false;
-		rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
-	}
-	
-	void FixedUpdate () {
-		// Allow this body to be influenced by planet's gravity
-		planet.Attract(rigidbody);
-	}
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        attractor.Attract(rb);
+    }
 }
